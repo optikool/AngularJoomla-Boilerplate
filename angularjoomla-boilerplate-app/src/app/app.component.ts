d673331a77b19368dae22ee49de5cf4c01075f7c
optikool <optikool@yahoo.com>
2019-12-03 23:31:38 -0800

    Added check for new content available. Commented out Videogular

--- angularjoomla-boilerplate-app/src/app/app.component.ts
+++ angularjoomla-boilerplate-app/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'angularjoomla-boilerplate-app';
+export class AppComponent implements OnInit {
+  title = 'angularjoomla-boilerplate-app: ' + Date.now().toString();
+
+  constructor(private router: Router, private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+    // Reload app if new content is available
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('New content available. Reload?')) {
+          this.router.routeReuseStrategy.shouldReuseRoute = () => true;
+          this.router.onSameUrlNavigation = 'reload';
+          this.router.navigate([this.router.url]);
+        }
+      });
+    }
+  }
 }
